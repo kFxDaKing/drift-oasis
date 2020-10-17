e2a831e65ae1fc76f4ad08d230b8c9bef043e6ee
REDRUM112 <zerkyoags@gmail.com>
2020-10-17 17:12:50 -0400

    Missing files.

---
+++ drift/fxmanifest.lua
@@ -0,0 +1,37 @@
+fx_version 'bodacious'
+game 'gta5'
+resource_type 'map' { gameTypes = { driftgametype = true } }
+this_is_a_map 'yes'
+map 'map.lua'
+
+data_file 'GTXD_PARENTING_DATA' 'meta/gtxd.meta'
+file 'meta/gtxd.meta'
+
+client_scripts {
+	'loadipl.lua',
+}
+
+files {
+----MAPS START HERE----
+}
+data_file 'DLC_ITYP_REQUEST' 'lookoutpoint.ytyp' -- NFS Canyon
+data_file 'DLC_ITYP_REQUEST' 'gokart.ytyp' -- Gokart
+data_file 'DLC_ITYP_REQUEST' 'harugaharav2.ytyp' -- Harugahara
+data_file 'DLC_ITYP_REQUEST' 'Minami.ytyp' -- Ebisu Minami
+data_file 'DLC_ITYP_REQUEST' 'EbisuTouge.ytyp' -- Ebisu Touge
+data_file 'DLC_ITYP_REQUEST' 'Phil.ytyp' -- Deriphill
+data_file 'DLC_ITYP_REQUEST' 'DriftNoodle.ytyp' -- Motorland
+data_file 'DLC_ITYP_REQUEST' 'SekiaHills.ytyp' -- Sekia Hills
+data_file 'DLC_ITYP_REQUEST' 'projtouge.ytyp' -- Project Touge
+data_file 'DLC_ITYP_REQUEST' 'dragon.ytyp' -- Tail of the Dragon
+data_file 'DLC_ITYP_REQUEST' 'kagara.ytyp' -- Kagarasan Track
+data_file 'DLC_ITYP_REQUEST' 'akinavvehicles.ytyp' -- AkinaV
+data_file 'DLC_ITYP_REQUEST' 'akinavtreeplus.ytyp' -- AkinaV
+data_file 'DLC_ITYP_REQUEST' 'akinavtree.ytyp' -- AkinaV
+data_file 'DLC_ITYP_REQUEST' 'akinavlights.ytyp' -- AkinaV
+data_file 'DLC_ITYP_REQUEST' 'akinavisland.ytyp' -- AkinaV
+data_file 'DLC_ITYP_REQUEST' 'akinavdetails.ytyp' -- AkinaV
+data_file 'DLC_ITYP_REQUEST' 'akinavdetailmid.ytyp' -- AkinaV
+data_file 'DLC_ITYP_REQUEST' 'akinavbridgedetails.ytyp' -- AkinaV
+data_file 'DLC_ITYP_REQUEST' 'akinavbridge.ytyp' -- AkinaV
+data_file 'DLC_ITYP_REQUEST' 'usui.ytyp' -- Usui
---
+++ drift/map.lua
@@ -0,0 +1,4 @@
+spawnpoint 'mp_m_freemode_01' { x = 3665, y = -355, z = 31 }
+spawnpoint 'mp_f_freemode_01' { x = 3665, y = -355, z = 31 }
+
+--
